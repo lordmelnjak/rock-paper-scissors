810ce480edabacee025a79a70a23e3c8f34216a3
Jurica Melnjak <melnjakjurica@gmail.com>
2022-10-08 10:05:12 +0200

    Made if else statement with choices.

--- js.js
+++ js.js
@@ -1,6 +1,31 @@
 
 let computerSelection = getComputerChoice()  //pozivam funkciju getComputerChoice na random generiranje
+let playerSelection = 'Rock';
 function getComputerChoice (){
     let choices = ['Rock','Paper','Scissors'];
     return choices[Math.floor(Math.random() * choices.length)]; //vracam izbor racunala
+}
+
+function playRound (playerSelection, computerSelection) {
+    let result;
+    if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
+        result = 'You win! Rock beats scissors!';
+    } else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
+        result = 'You loose! Paper beats rock!';
+    } else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
+        result = 'You win! Scissors beat paper!';
+    } else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
+        result = 'You loose! Rock beats Scissors!';
+    } else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
+        result = 'You win! Paper beats rock!';
+    } else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
+        result = 'You loose! Scissors beat paper!';
+    } else if (playerSelection == 'Rock' && computerSelection == 'Rock') {
+        result = 'Tie';
+    } else if (playerSelection == 'Paper' && computerSelection == 'Paper') {
+        result = 'Tie';
+    } else {
+        result = 'Tie';
+    }
+    return result;
 }
